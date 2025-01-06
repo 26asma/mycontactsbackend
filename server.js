c6e02a8b29550c9a6d285114ab50210baf9090ab
Asma <fathimasma26@gmail.com>
2025-01-06 10:26:35 +0530

    Added-CORS

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express =require("express");
+const cors = require("cors");
 const errorhandler = require("./middleware/errorhandler");
 const validateTokenHandler = require("./middleware/validateTokenHandler");
 const connectdb = require("./config/dbconnection");
@@ -7,6 +8,9 @@ connectdb();
 const app=express();
 const port = process.env.PORT|| 5000;
 app.use(express.json())
+app.use(cors({
+   origin: "*",  // Allows all origins temporarily for debugging
+ }));
 app.use("/api/contacts",require("./routes/contactroutes"));
 app.use("/api/users",require("./routes/userRoutes"));
 app.use(errorhandler)
